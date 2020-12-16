78a6c70218425283c2afe3ddc6c035dc0b6cc2fe
Theo Chan <chan.theodore1@gmail.com>
2020-12-16 09:32:18 -0800

    Deleted todo in mapit.py

--- webscraping/mapit.py
+++ webscraping/mapit.py
@@ -1,12 +1,18 @@
 # mapit.py - launches a map in a
 # browser using an address form the command line
 
-import webbrowser, sys
+import sys
+import webbrowser
+import pyperclip
+
+prefix = "https//www.google.com/maps/place/"
+
+# If there are arguments, use args as address
+# If there are no arguments, grab from clipboard
 
 if len(sys.argv) > 1:
     address = " ".join(sys.argv[1:])
-    prefix = "https//www.google.com/maps/place/"
+else:
+    address = pyperclip.paste()
 
-    webbrowser.open(prefix + address)
-
-# TODO: Get the address from the clipboard.
+webbrowser.open(prefix + address)
