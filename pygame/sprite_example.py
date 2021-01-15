f44717757eb42ffdaf53220ac969278f9e3092ad
Theo Chan <chan.theodore1@gmail.com>
2021-01-15 11:13:29 -0800

    sprite_example.py player created and controlled by mouse

--- pygame/sprite_example.py
+++ pygame/sprite_example.py
@@ -13,8 +13,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 YELLOW = (255, 255, 0)
 SKY_BLUE = (95, 165, 228)
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1920
+HEIGHT = 1080
 TITLE = "Sprite Example"
 NUM_JEWELS = 45
 
@@ -25,12 +25,25 @@ class Jewel(pygame.sprite.Sprite):
         super().__init__()
 
         # Image is a surface
-        self.image = pygame.Surface((35,20))
+        self.image = pygame.Surface((35, 20))
         self.image.fill((100, 255, 100))
 
         # Rect is Rectangle (x, y, width, height)
         self.rect = self.image.get_rect()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pygame.image.load("./images/link.png")
+
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        """Changes the position of the player
+        based on the mouse's position"""
+        self.rect.center = pygame.mouse.get_pos()
 
 
 
@@ -45,6 +58,7 @@ def main():
     # ----- LOCAL VARIABLES
     done = False
     clock = pygame.time.Clock()
+    score = 0
 
     # Sprite group and sprite creation
     all_sprites_group = pygame.sprite.Group()
@@ -56,6 +70,10 @@ def main():
         jewel.rect.y = random.randrange(HEIGHT - jewel.rect.height)
         all_sprites_group.add(jewel)
 
+    # Player creation
+    player = Player()
+    all_sprites_group.add(player)
+
     # ----- MAIN LOOP
     while not done:
         # -- Event Handler
@@ -64,6 +82,7 @@ def main():
                 done = True
 
         # ----- LOGIC
+        all_sprites_group.update()
 
         # ----- DRAW
         screen.fill(BLACK)
